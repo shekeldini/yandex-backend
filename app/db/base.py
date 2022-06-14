c59983239779272068e9ca793b67a25e9663ecf8
shekeldini <isayan1998@gmail.com>
2022-06-14 13:07:58 +0000

    delete redis password

--- app/db/base.py
+++ app/db/base.py
@@ -1,8 +1,8 @@
 from databases import Database
 from sqlalchemy import MetaData
-from app.core.config import DATABASE_URL, REDIS_URL, REDIS_PORT, REDIS_PASSWORD
+from app.core.config import DATABASE_URL, REDIS_URL, REDIS_PORT
 from redis import Redis
 
 metadata = MetaData()
 database = Database(DATABASE_URL)
-redis = Redis(host=REDIS_URL, port=REDIS_PORT, password=REDIS_PASSWORD)
+redis = Redis(host=REDIS_URL, port=REDIS_PORT)
